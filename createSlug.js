3c8fc0c6835f54340aa7b0f618fb6114f5ce7762
pietrostraneo <straneo_p@yahoo.it>
2024-05-30 17:51:20 +0200

    Added "test" script on package.json to execute tests, also created createSlug.js method and its test file.

---
+++ createSlug.js
@@ -0,0 +1,35 @@
+const slugify = require('slugify');
+const posts = require('./db/posts.js');
+
+module.exports = (title, arrayOfPosts) => {
+
+    // 4. `createSlug` dovrebbe incrementare di 1 lo slug quando esiste già.
+
+    // Creo lo slug
+    let slug = slugify(title).toLowerCase();
+
+    // Dichiario variabile uniqueSlug
+    let uniqueSlug = slug;
+    let count = 1
+
+    // Estraggo il primo post che abbia lo slug uguale a quello unico
+    while (posts.some(post => post.slug === uniqueSlug)) {
+
+        // Genero uno slug nuovo
+        uniqueSlug = `${slug}-${count++}`;
+    }
+
+
+    // 5. `createSlug` dovrebbe lanciare un errore in caso di titolo non presente o formato errato.
+    if (typeof title !== 'string' || title.trim() === '') {
+        throw new Error('Title is required and must be of type string');
+    }
+
+
+    // 6. `createSlug` dovrebbe lanciare un errore se manca l'array dei post.
+    if (!arrayOfPosts) {
+        throw new Error('Array of posts is required');
+    }
+
+    return uniqueSlug;
+}
---
+++ createSlug.test.js
@@ -0,0 +1,33 @@
+const createSlug = require('./createSlug.js');
+const posts = require('./db/posts.js');
+
+
+test('dovrebbe ritornare una stringa', () => {
+    const result = createSlug('Titolo di Test', posts);
+    expect(typeof result).toBe('string');
+})
+
+test('dovrebbe ritornare una stringa in lowercase', () => {
+    const result = createSlug('Titolo di Test', posts);
+    expect(result).toBe('titolo-di-test');
+})
+
+test('dovrebbe ritornare una stringa con gli spazi sostituiti da `-`', () => {
+    const result = createSlug('Titolo di Test', posts);
+    expect(result).toBe('titolo-di-test');
+})
+
+test('dovrebbe incrementare di 1 lo slug quando esiste già.', () => {
+    const result = createSlug('Cracker alla barbabietola', posts);
+    expect(result).toBe('cracker-alla-barbabietola-1');
+})
+
+test('dovrebbe lanciare un errore in caso di titolo non presente o formato errato', () => {
+    expect(() => createSlug('', [])).toThrow();
+    expect(() => createSlug(123, [])).toThrow();
+})
+
+test('dovrebbe lanciare un errore se manca l\'array dei post.', () => {
+    expect(() => createSlug('Titolo di Test')).toThrow();
+})
+
